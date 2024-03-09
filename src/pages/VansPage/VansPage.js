322131cfe153fbe37d8fcb5d28e6168f3be43266
Neelesh Joshi <neeleshjoshi001@gmail.com>
2024-03-09 16:29:02 +0530

    #added filter functionality using useSearchParams()

--- src/pages/VansPage/VansPage.js
+++ src/pages/VansPage/VansPage.js
@@ -4,10 +4,17 @@ import { useState, useEffect } from 'react';
 import '../../server';
 import axios from 'axios';
 import './index.css'
+import { useSearchParams } from 'react-router-dom';
 
 const VansPage = () => {
 
   const [vans,setVans] = useState([])
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const filterType = searchParams.get("type");
+  
+  const finalFilteredVans = filterType ? vans.filter(eachVan => eachVan.type === filterType) : vans;
+  console.log(finalFilteredVans);
 
   useEffect(()=>{
     axios.get('/vans/data')
@@ -17,8 +24,9 @@ const VansPage = () => {
   ,[])
 
 
-  const vansArr = vans.map((van,index)=>{
+  const vansArr = finalFilteredVans.map((van,index)=>{
     return (   <Vans 
+      key={index}
       vanId = {index}
       imageUrl = {van.image_url}
       name = {van.name}
@@ -35,6 +43,12 @@ const VansPage = () => {
       <h2 className="loading">Loading.....</h2>:
       <>
         <h1 className="van-options">Explore our van options</h1>
+        <div className='filters-container'>
+          <button className={'van-filter-buttons simple'} onClick={()=>{setSearchParams({type: "simple"})}}>simple</button>
+          <button className={'van-filter-buttons luxury'} onClick={()=>{setSearchParams({type: "luxury"})}}>luxury</button>
+          <button className={'van-filter-buttons rugged'} onClick={()=>{setSearchParams({type: "rugged"})}}>rugged</button>
+          <button className={'van-filter-buttons clear'} onClick={()=>{setSearchParams({})}}>clear</button>
+        </div>
           <div className="vans-container">
             {vansArr}
           </div>
